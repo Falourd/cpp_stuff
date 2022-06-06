206c8f07196fe94127b09c83c5880797d39380cd
Falourd <david.letoux@gmail.com>
2022-06-06 21:15:16 +0000

    - init configuration du projet

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "Hello Falourd\n";
+    return 0;
+}
---
+++ test/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "Hello Test\n";
+    return 0;
+}
